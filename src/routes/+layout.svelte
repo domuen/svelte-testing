234c9880c2e9f332c9ee9c1541473dfe3965baa0
= <=>
2023-10-02 22:06:44 -0400

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,30 @@
   <h1>Svelte Testing</h1>
 </div>
 
-<slot />
+<div class="slot">
+  <slot />
+</div>
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap");
+
+  :global(body) {
+    margin: 0;
+    background-color: #212121;
+    font-family: "Montserrat", sans-serif;
+  }
+
+  :global(h1, h2, h3, h4, h5, h6, p) {
+    margin: 0;
+    color: #fff;
+  }
+
   .container {
-    background-color: grey;
-    padding: 10px;
+    background-color: #131313;
+    padding: 20px;
+  }
+
+  .slot {
+    padding: 20px;
   }
 </style>
