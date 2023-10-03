2bd6a7ba54f58e5a4e3dc39bebfc57a49e48c046
= <=>
2023-10-02 22:26:07 -0400

    install tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,14 @@
-<div class="container">
+<script>
+  import "../app.css";
+</script>
+
+<div class="p-8 bg-[#131313]">
   <h1>Svelte Testing</h1>
 </div>
 
-<div class="slot">
+<div class="p-8">
   <slot />
 </div>
 
 <style>
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap");
-
-  :global(body) {
-    margin: 0;
-    background-color: #212121;
-    font-family: "Montserrat", sans-serif;
-  }
-
-  :global(h1, h2, h3, h4, h5, h6, p) {
-    margin: 0;
-    color: #fff;
-  }
-
-  .container {
-    background-color: #131313;
-    padding: 20px;
-  }
-
-  .slot {
-    padding: 20px;
-  }
 </style>
