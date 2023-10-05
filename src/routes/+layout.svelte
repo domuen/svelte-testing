04aa7b367f300ae8b0383991157920dfae50ddeb
= <=>
2023-10-05 11:10:16 -0400

    make header clickable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script>
   import "../app.css";
+  import { goto } from "$app/navigation";
+
+  const handleNavigate = () => {
+    goto("/");
+  };
 </script>
 
 <div class="p-8 bg-[#131313]">
-  <h1>Svelte Testing</h1>
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
+  <h1 class="cursor-pointer select-none" on:click={handleNavigate}>
+    Svelte Testing
+  </h1>
 </div>
 
 <div class="p-8">
