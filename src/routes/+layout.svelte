67922839dd49a11c70128ab2dcd508db7f5e4f3f
= <=>
2023-10-05 13:20:04 -0400

    update page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,59 @@
 <script>
   import "../app.css";
   import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
+
+  const routeHeader = (() => {
+    const { pathname } = $page.url;
+
+    if (pathname === "/") return "Home";
+    if (pathname === "/components") return "Components";
+
+    return pathname;
+  })();
+
+  const routes = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Components",
+      path: "/components",
+    },
+    {
+      name: "Items",
+      path: "/items",
+    }
+  ];
 
   const handleNavigate = () => {
     goto("/");
   };
 </script>
 
-<div class="p-8 bg-[#131313]">
-  <!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
-  <h1 class="cursor-pointer select-none" on:click={handleNavigate}>
-    Svelte Testing
-  </h1>
-</div>
+<div class="flex flex-row">
+  <div class="flex flex-col h-screen border-r border-r-secondary">
+    <div class="bg-primary border-b border-b-secondary px-48 py-10">
+      <h1 class="whitespace-nowrap">Svelte Testing</h1>
+    </div>
 
-<div class="p-8">
-  <slot />
+    <div class="flex flex-col p-6 gap-6">
+      {#each routes as route}
+        <a class="text-button-text" href={route.path}>{route.name}</a>
+      {/each}
+    </div>
+  </div>
+
+  <div class="flex flex-col w-full">
+    <div
+      class="flex items-center justify-center bg-primary border-b border-b-secondary px-48 py-10"
+    >
+      <h1 class="whitespace-nowrap">{routeHeader}</h1>
+    </div>
+
+    <div class="p-6">
+      <slot />
+    </div>
+  </div>
 </div>
