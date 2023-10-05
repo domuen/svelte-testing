4fbf2c6c022481c28a4c31f82cb136e446a1d7f9
= <=>
2023-10-05 12:09:35 -0400

    create component gallery with auth components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,11 @@
 <script lang="ts">
-  import { userWritable } from "$lib/store.js";
-  import { goto } from "$app/navigation";
-
-  let user: any = undefined;
   const date = 1696216682088;
-
-  userWritable.subscribe((v) => (user = v));
-
-  const handleNavigate = () => {
-    goto("/login");
-  };
-
-  const handleLogout = () => {
-    userWritable.set(undefined);
-  };
 </script>
 
 <div class="flex flex-col items-center justify-center">
   <h1>Homepage</h1>
 
-  {#if !Boolean(user?.name)}
-    <p>Welcome to my svelte testing app</p>
-  {:else}
-    <p>Welcome back, {user?.name}, to my svelte testing app</p>
-  {/if}
+  <h3>Welcome to my svelte testing app</h3>
 
   <p>
     I'm going to figure this framework out and see if I'd want to use it in a
@@ -31,11 +13,4 @@
   </p>
 
   <p>I began this journey on {new Date(date)}</p>
-
-  {#if !Boolean(user)}
-    <h3>You are currently not logged in</h3>
-    <button on:click={handleNavigate}>Login</button>
-  {:else}
-    <button on:click={handleLogout}>Logout</button>
-  {/if}
 </div>
---
+++ src/routes/components/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Login from "$lib/components/login.svelte";
+  import Register from "$lib/components/register.svelte";
+</script>
+
+<div class="flex flex-col items-center justify-center gap-8">
+  <h1>Components</h1>
+
+  <p>These are some common components I made</p>
+
+  <div class="flex flex-row items-center gap-8">
+    <div
+      class="flex flex-col items-center border-[#3c3c3c] border rounded-xl overflow-hidden"
+    >
+      <div class="bg-[#303030] w-full">
+        <h3 class="px-6 py-3">Login</h3>
+      </div>
+
+      <div class="bg-[#242424] p-10">
+        <Login />
+      </div>
+    </div>
+
+    <div
+      class="flex flex-col items-center border-[#3c3c3c] border rounded-xl overflow-hidden"
+    >
+      <div class="bg-[#303030] w-full">
+        <h3 class="px-6 py-3">Register</h3>
+      </div>
+
+      <div class="bg-[#242424] p-10">
+        <Register />
+      </div>
+    </div>
+  </div>
+</div>
